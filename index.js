6ef236f8c47c05b12a3efbd73c5c520317161a5a
makanindian <makanindian@gmail.com>
2025-07-24 11:12:57 +0530

    First Commit

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ app.get("/api/questions", async (req, res) => {
 });
 
 // ✅ Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
